332e83ae22c4257ab91587c82f688dc6f2132b1c
brettsteph <cloudengineer777@gmail.com>
2023-03-24 19:47:44 -0400

    Remove unused variables

--- variables.tf
+++ variables.tf
@@ -1,48 +1,26 @@
 variable "gcp_region" {
   description = "GCP region"
   type        = string
-}
-variable "gcp_zone" {
-  description = "GCP zone"
-  type        = string
+  default     = "us-central1"
 }
 variable "project_id" {
   description = "The GCP project id"
   type        = string
 }
-variable "namespace" {
-  description = "The namespace for resource naming"
+variable "docker_image_name" {
+  description = "Docker image name"
   type        = string
 }
-
-variable "gcp_bucket_name" {
-  description = "Storage bucket name"
-  type        = string
-}
-
-variable "image_name" {
-  description = "Storage bucket name"
-  type        = string
-}
-
 variable "image_tag" {
   description = "Storage bucket name"
   type        = string
 }
-
-variable "bucket_name" {
-  type        = string
-  description = "The name of the test log bucket"
-}
-
 variable "github_repository" {
   type = string
 }
-
 variable "github_owner" {
   type = string
 }
-
 variable "github_branch" {
   type = string
 }
